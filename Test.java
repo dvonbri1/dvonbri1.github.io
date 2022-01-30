478d36985b41c55e868df0fceaf06e7aefc1b8e5
divonbri1 <divonbri1@uncc.edu>
2022-01-30 15:41:30 -0500

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test
+{
+   public static void main(String[] args)
+   {
+       System.out.print("Hi Patricia. It's working!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ");
+       System.out.println("there");
+       System.out.print("!");
+   }
+}
